1e269a32e6845809b1d8e6cab767044702abdce4
haseeAmarathunga <haseeamarathunga@gmail.com>
2019-08-22 12:29:19 +0530

    Update customer table

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {CustomerComponent} from './customer/customer.component';
+import {CustomerListComponent} from './customer-list/customer-list.component';
+import {HomeComponent} from './home/home.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path : 'home', component: HomeComponent},
+  {path : 'customer', component : CustomerComponent},
+  {path : 'customerlist', component : CustomerListComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
